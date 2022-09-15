eb4e657096bac5b85f1343e82a4dfbc0ae3b4919
Nika <nikarasevas@gmail.com>
2022-09-15 22:46:24 +0300

    Цифра для дня недели

---
+++ Задача_3/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите программу, которая будет выдавать название 
+дня недели по заданному номеру.
+3 -> Среда
+5 ->  Пятница*/
+
+Console.WriteLine("Введите день недели");
+int day = int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine("понедельник");
+}
+if (day == 2)
+{
+    Console.WriteLine("вторник");
+}
+if (day == 3)
+{
+    Console.WriteLine("среда");
+}
+if (day == 4)
+{
+    Console.WriteLine("четверг");
+}
+if (day == 5)
+{
+    Console.WriteLine("пятница");
+}
+if (day == 6)
+{
+    Console.WriteLine("суббота");
+}
+if (day == 7)
+{
+    Console.WriteLine("воскресенье");
+}
